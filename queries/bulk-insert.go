34545b97f91d85633f6408a330e1c20b75164bee
Muhammad Ziad Rahmatullah <ziad.rahmatullah@shopee.com>
2023-11-11 16:40:46 +0700

    feat: cleaning code

--- queries/bulk-insert.go
+++ queries/bulk-insert.go
@@ -9,32 +9,8 @@ import (
 )
 
 func BulkInsert(db *sql.DB, products []*entity.Product) error {
-    // valueStrings := make([]string, 0, len(products))
-    // valueArgs := make([]interface{}, 0, len(products) * 3)
-    // i := 0
-    // for _, product := range products {
-    //     valueStrings = append(valueStrings, fmt.Sprintf("(@p%d, @p%d, @p%d, @p%d)", i*3+1, i*3+2, i*3+3, i*3+4))
-    //     valueArgs = append(valueArgs, product.Name)
-    //     valueArgs = append(valueArgs, product.Price)
-    //     valueArgs = append(valueArgs, product.ProductCategoryId)
-    //     valueArgs = append(valueArgs, product.Stock)
-    //     i++
-    // }
-    // sqlQuery := fmt.Sprintf("INSERT INTO products (name, price, category_id, stock) VALUES %s", strings.Join(valueStrings, ","))
-
-    // var params []interface{}
-
-    // for i := 0; i < len(valueArgs); i++ {
-    //     var param sql.NamedArg
-    //     param.Name = fmt.Sprintf("p%v", i+1)
-    //     param.Value = valueArgs[i]
-    //     params = append(params, param)
-    // }
-
-    // _, err := db.Exec(sqlQuery, params...)
-    // return err
 	valueStrings := make([]string, 0, len(products))
-    valueArgs := make([]interface{}, 0, len(products)*4) // Updated to *4 for each parameter
+    valueArgs := make([]interface{}, 0, len(products)*4)
     i := 0
     for _, product := range products {
         valueStrings = append(valueStrings, fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d)", i*6+1, i*6+2, i*6+3, i*6+4, i*6+5, i*6+6))
